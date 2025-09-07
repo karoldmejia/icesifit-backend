add49a64dc98cb2a2962f210bdd1b4b7b88af1fe
Heiner Rincón <heinerdanitrincon@gmail.com>
2025-09-07 18:05:52 -0500

    add: project structure

---
+++ physical_activity_project/src/test/java/com/example/physical_activity_project/PhysicalActivityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.physical_activity_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalActivityProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
